bac8d3829245807165da3ca7cd5829515d8eda05
Dhruv kumar <134788310+dK1925402@users.noreply.github.com>
2024-12-02 06:18:21 +0530

    Update HeroSection.jsx

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -19,7 +19,7 @@ const HeroSection = () => {
         <div className='text-center'>
             <div className='flex flex-col gap-5 my-10'>
                 <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
-                <h1 className='text-5xl font-bold'>Sarvagya Nirakar Community <br /> <br />For Our <span className='text-[#6A38C2]'>Better Future</span></h1>
+                <h1 className='text-5xl font-bold'>Sarvagya Nirakar Community <br /> For Our <span className='text-[#6A38C2]'>Better Future</span></h1>
                 <p>Our Mission - Youth Empowerment for Nation Building</p>
                 <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input
