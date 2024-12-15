4dc3c174a3d1dc5cc4670891ee2d533ec1d77527
Dhruv kumar <134788310+dK1925402@users.noreply.github.com>
2024-12-15 19:49:59 +0530

    Responsive Update

--- frontend/src/components/AppliedJobTable.jsx
+++ frontend/src/components/AppliedJobTable.jsx
@@ -8,7 +8,7 @@ const AppliedJobTable = () => {
     return (
         <div>
             <Table>
-                <TableCaption>A list of your applied jobs</TableCaption>
+                <TableCaption className='m-5 p-5'>A list of your applied jobs</TableCaption>
                 <TableHeader>
                     <TableRow>
                         <TableHead>Date</TableHead>
@@ -19,7 +19,7 @@ const AppliedJobTable = () => {
                 </TableHeader>
                 <TableBody>
                     {
-                        allAppliedJobs.length <= 0 ? <span>You haven't applied any job yet.</span> : allAppliedJobs.map((appliedJob) => (
+                        allAppliedJobs.length <= 0 ? <span  >You haven't applied any job yet.</span> : allAppliedJobs.map((appliedJob) => (
                             <TableRow key={appliedJob._id}>
                                 <TableCell>{appliedJob?.createdAt?.split("T")[0]}</TableCell>
                                 <TableCell>{appliedJob.job?.title}</TableCell>
